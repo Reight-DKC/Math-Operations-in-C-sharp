62dd3292ba332243de3a673060d778344758f7a0
Reight-DKC <reightultimate@gmail.com>
2022-06-10 10:53:47 -0600

    Math practice complete

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// Operaciones matemáticas
+
+// Suma
+Console.WriteLine(10 + 15);
+Console.WriteLine(30 + 45);
+
+// Resta
+Console.WriteLine(45 - 20);
+Console.WriteLine(60 - 15);
+
+// Multiplicación
+Console.WriteLine(50 * 2);
+Console.WriteLine(35 * 3);
+
+// Division
+Console.WriteLine(Decimal.Divide(20,3));
+Console.WriteLine(Decimal.Divide(70,6));
+
+// Exponente
+Console.WriteLine(Math.Pow(35, 3));
+Console.WriteLine(Math.Pow(41, 4));
+
+// Division entera
+Console.WriteLine(55 / 5);
+Console.WriteLine(70 / 7);
+
+// Módulo
+Console.WriteLine(50 % 5);
+Console.WriteLine(20 % 2);
